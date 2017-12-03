3fe335d959f8d77731e915f33ce4e3e53d4de49c
KCaasi <hero.santos2017@yandex.com>
2017-12-03 15:12:02 +0800

    FRUIT

---
+++ reactMinimal-master/www/js/process.js
@@ -0,0 +1,300 @@
+  var lavender =
+
+<div>
+
+      <div className="swiper-slide"><img src="img/CHERRY.jpg" />
+        <div className="parent_div">  
+<button className="button">CHERRY
+</button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/APRICOT.jpg" />
+        <div className="parent_div">
+          <button className="button">APRICOT
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/date.jpg" />
+        <div className="parent_div">
+          <button className="button">DATE
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/apple.jpg" />
+        <div className="parent_div">
+          <button className="button">APPLE
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/jackfruit.png" />
+        <div className="parent_div">
+          <button className="button">JACKFRUIT
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/lime.jpg" />
+        <div className="parent_div">
+          <button className="button">LIME
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/lemon.jpg" />
+        <div className="parent_div">
+          <button className="button">LEMON
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/papaya.jpg" />
+        <div className="parent_div">
+          <button className="button">PAPAYA
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/balckcurrant.jpg" />
+        <div className="parent_div">
+          <button className="button">BLACKCURRANT
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/lychee.jpg" />
+        <div className="parent_div">
+          <button className="button">LYCHEE
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/peach.jpg" />
+        <div className="parent_div">
+          <button className="button">PEACH
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/strawberry.jpg" />
+        <div className="parent_div">
+          <button className="button">STRAWBERRY
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/mango.jpg" />
+        <div className="parent_div">
+          <button className="button">MANGO
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/mangosteen.jpg" />
+        <div className="parent_div">
+          <button className="button">MANGOSTEEN
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/melon.jpg" />
+        <div className="parent_div">
+          <button className="button">CANTALOUPE
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/orange.jpg" />
+        <div className="parent_div">
+          <button className="button">ORANGE
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/sapota.jpg" />
+        <div className="parent_div">
+          <button className="button">SAPOTA
+          </button></div>
+      </div>
+      <div className="swiper-slide"><img src="img/watermelon.jpg" />
+        <div className="parent_div">
+          <button className="button">WATERMELON
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/pomegrenate.jpg" />
+        <div className="parent_div">
+          <button className="button">POMEGRENATE
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/hazelnut.jpg" />
+        <div className="parent_div">
+          <button className="button">HAZELNUT
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/WOODAPPLE.jpg" />
+        <div className="parent_div">
+          <button className="button">WOODAPPLE
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/grapes.jpg" />
+        <div className="parent_div">
+          <button className="button">GRAPES
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/guava.jpg" />
+        <div className="parent_div">
+          <button className="button">GUAVA
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/custardapple.png" />
+        <div className="parent_div">
+          <button className="button">CUSTARDAPPLE
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/Banana.jpg" />
+        <div className="parent_div">
+          <button className="button">BANANA
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/kiwi.jpg" />
+        <div className="parent_div">
+          <button className="button">KIWI
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/chestnut.png" />
+        <div className="parent_div">
+          <button className="button">CHESTNUT
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/COCONUT1.jpg" />
+        <div className="parent_div">
+          <button className="button">COCONUT
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/cashew.jpg" />
+        <div className="parent_div">
+          <button className="button">CASHEW
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/walnut.jpg" />
+        <div className="parent_div">
+          <button className="button">WALNUT
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/pear.jpg" />
+        <div className="parent_div">
+          <button className="button">PEAR
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/waterchestnut.jpeg" />
+        <div className="parent_div">
+          <button className="button">WATER CHESTNUT
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/raspberry.jpg" />
+        <div className="parent_div">
+          <button className="button">RASPBERRY
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/pineapple.jpg" />
+        <div className="parent_div">
+          <button className="button">PINEAPPLE
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/olive.jpg" />
+        <div className="parent_div">
+          <button className="button">OLIVE
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/turnips.png" />
+        <div className="parent_div">
+          <button className="button">TURNIPS
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/peanut.png" />
+        <div className="parent_div">
+          <button className="button">PEANUT
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/quince.jpg" />
+        <div className="parent_div">
+          <button className="button">QUINCE
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/gooseberries.jpg" />
+        <div className="parent_div">
+          <button className="button">GOOSEBERRIES
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/avocado.png" />
+        <div className="parent_div">
+          <button className="button">AVOCADO
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/almond.jpg" />
+        <div className="parent_div">
+          <button className="button">ALMOND
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/dragonfruit.jpg" />
+        <div className="parent_div">
+          <button className="button">DRAGONFRUIT
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/acorn.jpg" />
+        <div className="parent_div">
+          <button className="button">ACORN
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/plum.jpg" />
+        <div className="parent_div">
+          <button className="button">PLUM
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/blueberry.png" />
+        <div className="parent_div">
+          <button className="button">BLUEBERRY
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/cucumber.png" />
+        <div className="parent_div">
+          <button className="button">CUCUMBER
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/cranberry.png" />
+        <div className="parent_div">
+          <button className="button">CRANBERRY
+          </button>
+        </div>
+      </div> 
+      <div className="swiper-slide"><img src="img/blackberry.jpg" />
+        <div className="parent_div">
+          <button className="button">BLACKBERRY
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/corn.jpg" />
+        <div className="parent_div">
+          <button className="button">CORN
+          </button></div>  
+      </div>
+ 
+    </div>
+  
+ReactDOM.render(lavender,document.getElementById('root'));
